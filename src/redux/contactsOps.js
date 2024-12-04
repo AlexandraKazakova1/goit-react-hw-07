144505b1951a507221fd51389759fd1bfce29e24
Alex <zemzikova.oleksandra@gmail.com>
2024-12-04 19:59:27 +0200

    ops

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.default.baseURL = "https://6750880f69dc1669ec1b87c1.mockapi.io";
+axios.defaults.baseURL = "https://6750880f69dc1669ec1b87c1.mockapi.io";
 
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
@@ -15,7 +15,7 @@ export const fetchContacts = createAsyncThunk(
   }
 );
 
-export const addContacts = createAsyncThunk(
+export const addContact = createAsyncThunk(
   "contacts/addContact",
   async (body, thunkAPI) => {
     try {
@@ -27,7 +27,7 @@ export const addContacts = createAsyncThunk(
   }
 );
 
-export const deleteContacts = createAsyncThunk(
+export const deleteContact = createAsyncThunk(
   "contacts/deleteContact",
   async (id, thunkAPI) => {
     try {
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,17 +1,53 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchContacts } from "./contactsOps";
 
 const contactsSlice = createSlice({
   name: "contacts",
   initialState: { items: [] },
-  reducers: {
-    addContact: (state, action) => {
-      state.items.push(action.payload);
-    },
-    deleteContact: (state, action) => {
-      state.items = state.items.filter(
-        (contact) => contact.id !== action.payload
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchContacts.fulfilled, (state, { payload }) => {
+        state.items = payload;
+      })
+      .addCase(addContact.fulfilled, (state, { payload }) => {
+        state.items.push(payload);
+      })
+      .addCase(deleteContact.fulfilled, (state, { payload }) => {
+        state.items = state.items.filter((item) => item.id !== payload);
+      })
+      .addMatcher(
+        isAnyOf(
+          addContact.pending,
+          deleteContact.pending,
+          fetchContacts.pending
+        ),
+        (state) => {
+          state.isError = false;
+          state.isLoading = true;
+        }
+      )
+
+      .addMatcher(
+        isAnyOf(
+          addContact.rejected,
+          deleteContact.rejected,
+          fetchContacts.rejected
+        ),
+        (state) => {
+          state.isLoading = false;
+          state.isError = true;
+        }
+      )
+      .addMatcher(
+        isAnyOf(
+          addContact.fulfilled,
+          deleteContact.fulfilled,
+          fetchContacts.fulfilled
+        ),
+        (state) => {
+          state.isLoading = false;
+        }
       );
-    },
   },
 });
 
